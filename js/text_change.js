f218936e066f099633745218d053df0ab343f3e3
Hong-Eungyo <vjvlaos75@naver.com>
2024-01-15 18:09:15 +0900

    20240115
    winter season 상품까지

--- js/text_change.js
+++ js/text_change.js
@@ -2,10 +2,8 @@ const changeMenu = document.querySelector('#menu_01_over');
 
 changeMenu.addEventListener('mouseover', ()=>{
     changeMenu.innerText = '자체제작';
-    changeMenu.style.color = 'red';
 })
 
 changeMenu.addEventListener('mouseout',()=>{
     changeMenu.innerText = 'MADE.J';
-    changeMenu.style.color = '#333';
 })
